7f940e72d5d7926736d1f26a9511dc26ef1ec3c5
JunKyle <claudine.manrique@gmail.com>
2023-08-15 11:49:31 +0200

    images and text presentation

--- js/data.js
+++ js/data.js
@@ -18,15 +18,37 @@ function ajax_get(url, callback) {
 
 function getData (slug) {
     ajax_get('./assets/data/' + slug + '.json', function(data) {
-        var htmlArt = '';
-        var art = data[slug];
+        let htmlArt = '';
+        let art = data[slug];
         if (art && art.length > 0) {
-            for (var i=0; i < art.length; i++) {
+            for (let i=0; i < art.length; i++) {
                 htmlArt += '<a class="ArtPage__item" href="' + art[i].link +'">';
-                htmlArt += '<img class="ArtPage__image" src="' + art[i].link  + '" alt="' + art[i].alternative  + '" title="' + art[i].name  + '"/>';
+                htmlArt += '<img class="ArtPage__image" src="' + art[i].image  + '" alt="' + art[i].alternative  + '" title="' + art[i].name  + '"/>';
                 htmlArt += '</a>';
             }
         }
         document.getElementById(slug + "-art").innerHTML = htmlArt;
     });    
+}
+
+function getSingleData () {
+    const queries = window.location.search;
+    const urlParams = new URLSearchParams(queries);
+    const slug = urlParams.get('slug');
+    const id = urlParams.get('id');
+    ajax_get('./assets/data/' + slug + '.json', function(data) {
+        const art = data[slug];
+        if (art && art.length > 0) {
+            const item = art.find( element => element.id.toString() === id);
+            if (item) {
+                document.getElementById("single-art-title").innerHTML = item.title;
+                let htmlArt = '';
+                htmlArt += "<div class='SingleArt__carousel'>"
+                htmlArt += '<img class="SingleArt__image" src="' + item.image  + '" alt="' + item.alternative  + '" title="' + item.name  + '"/>';
+                htmlArt += "</div>";
+                htmlArt += '<span class="SingleArt__description">' + item.description + "</span>";
+                document.getElementById("single-art-section").innerHTML = htmlArt;
+            }
+        }
+    });   
 }
